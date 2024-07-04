48be1e43b78ea3c4f4be57075c7c7c97f79d9a5a
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2024-07-04 11:22:15 +0530

    Added Connection to Database function

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js"
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Connection Established Successfully At:${connectionInstance.connection.host}`);
+    }
+    catch(error){
+        console.error("Connection to Database Failed:",error);
+        process.exit(1);
+    }
+}
+export default connectDB;
