56d67b7586d6f2c900bf99a989190eff8c85170d
SHUBHSAXENA\shubh <shubhsaxena447@gmail.com>
2024-07-07 00:27:49 +0530

    Added Cloudinary and Multer utils and middlewares

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (handlerRequest) => {
-    (req,res,next) => {
+    return (req,res,next) => {
     Promise.resolve(handlerRequest(req,res,next)).catch((err) => next(err))
     }
 }
